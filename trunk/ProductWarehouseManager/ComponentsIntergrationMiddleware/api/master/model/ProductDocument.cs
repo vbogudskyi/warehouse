833eba5ef922202aa78579b475ad2ad40eccc065
Vitalii Bohudskyi <vbigudskyi@gmail.com>
2018-04-19 09:44:29 -0400

    DB Script is updated, new models are added

---
+++ trunk/ProductWarehouseManager/ComponentsIntergrationMiddleware/api/master/model/ProductDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentsIntergrationMiddleware.api.master.model
+{
+    sealed class ProductDocument
+    {
+        private int id;
+        private String documentId;
+        private String name;
+        private String prId;
+    }
+}
---
+++ trunk/ProductWarehouseManager/ComponentsIntergrationMiddleware/api/master/model/ProductType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentsIntergrationMiddleware.api.master.model
+{
+    sealed class ProductType
+    {
+        private int tid;
+        private String name;
+
+        public int TID
+        {
+            get { return tid; }
+            set { tid = value; }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+}
