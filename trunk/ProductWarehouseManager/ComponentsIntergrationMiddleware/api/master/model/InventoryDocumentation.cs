9c2631b1fa051ea45f2d805e856ed16010ac80c8
Nicolae <nastas_colea@yahoo.com>
2018-04-20 03:43:07 -0400

    message

---
+++ trunk/ProductWarehouseManager/ComponentsIntergrationMiddleware/api/master/model/InventoryDocumentation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentsIntergrationMiddleware.api.master.model
+{
+    sealed class InventoryDocumentation
+    {
+        private int id;
+        private String uid;
+        private String documentId;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public String UID
+        {
+            get { return uid; }
+            set { uid = value; }
+        }
+
+        public String DocumentID
+        {
+            get { return documentId; }
+            set { documentId = value; }
+        }
+    }
+}
